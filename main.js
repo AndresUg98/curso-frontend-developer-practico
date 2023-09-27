a5efa1f4edfc701b11cfc0d4137558faab445cec
AndresUg98 <oscaran2109@gmail.com>
2023-09-27 13:35:59 -0600

    adding products

--- main.js
+++ main.js
@@ -2,16 +2,100 @@ const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuBurguerIcon = document.querySelector(".menu");
+const navbarShoppingCart = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+
+const cardsContainer = document.querySelector(".cards-container");
 
 // Escuchamos el evento click de las de los siguientes elementos mediante su clase css
 navbarEmail.addEventListener("click", toggleDesktopMenu);
 menuBurguerIcon.addEventListener("click", toggleMobileMenu);
+navbarShoppingCart.addEventListener("click", toggleCarritoAside);
 
 // Funciones que nos ayudan a aparecer y desaparecer el elemnto gracias a la clase 'inactive'
 function toggleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
   mobileMenu.classList.toggle("inactive");
 }
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+  aside.classList.toggle("inactive");
+}
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Nintendo Switch",
+  price: 5500,
+  image: "https://m.media-amazon.com/images/I/61dYrzvBLbL.jpg",
+});
+productList.push({
+  name: "Xbox Series X",
+  price: 9899,
+  image:
+    "https://assets.xboxservices.com/assets/fb/d2/fbd2cb56-5c25-414d-9f46-e6a164cdf5be.png?n=XBX_A-BuyBoxBGImage01-D.png",
+});
+
+function renderProdutcs(arr) {
+  for (product of arr) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+
+    const productName = document.createElement("p");
+    productName.innerText = "$" + product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement("figure");
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+renderProdutcs(productList);
